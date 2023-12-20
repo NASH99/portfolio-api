7dfd47efe4482fbe73c54e66131626ff463e1614
NASH <nash@NASHs-iMac.local>
2023-12-20 12:08:06 -0300

    comment

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const router = express.Router();
 
 app.use(express.json());
 
+//CORS
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
